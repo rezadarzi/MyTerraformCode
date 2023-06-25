1570d5aacdd4457d5d477d65ea1a94ffd764f265
Reza Darzi <31057674+rezadarzi@users.noreply.github.com>
2023-06-25 17:10:19 +0330

    Add files via upload

---
+++ Consul/registerExternalNode/variables.tf
@@ -0,0 +1,24 @@
+variable  "consulAddress"{
+  default =  "192.168.1.124"
+}
+
+variable  "dc"{
+  default =  "dc1"
+}
+
+variable  "nodeName"{
+  default =  "TESTnode"
+}
+
+variable  "nodeAddress"{
+  default =  "localhost"
+}
+
+variable  "serviceName"{
+  default =  "TESTnode"
+}
+
+variable  "portNumber"{
+  default =  8080
+}
+
