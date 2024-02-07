8994555c4a852fe63d5e3fd9b8301c0fb71df845
Reza Darzi <rd.darzi@gmail.com>
2024-02-07 12:50:39 +0330

    Create variables.tf

---
+++ KodeKloudChallenges/Challenge3/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = string
+  default = "eu-west-2"
+}
+variable "instance_type" {
+  type = string
+  default = "m5.large"
+}
+variable "ami" {
+  type = string
+  default = "ami-06178cf087598769c"
+}
